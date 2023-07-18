a98774450cde22d66f557387168422dff754b817
kasinaat.ss <kasinaat007@gmail.com>
2023-07-18 19:08:09 +0530

    sqlite removed and mysql adopted

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,22 @@
+/* eslint-disable indent */
+/* eslint-disable no-tabs */
 const Sequelize = require('sequelize');
 
 const logger = require('./logger-util');
 
-const dbConnection = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-});
-
+const dbConnection = new Sequelize(
+	'site_monitor',
+	'root',
+	'',
+	{
+		host: 'localhost',
+		dialect: 'mysql',
+	},
+);
 dbConnection.authenticate().then(() => {
-  logger.getLogger('general').info('Connection has been established');
+	logger.getLogger('general').info('Connection has been established');
 }).catch(() => {
-  logger.getLogger('general').info('Failed to connect to DB');
+	logger.getLogger('general').info('Failed to connect to DB');
 });
 
 const site = require('../models/sites');
@@ -18,5 +24,5 @@ const site = require('../models/sites');
 const dbSite = site(dbConnection);
 
 module.exports = {
-  site: dbSite,
+	site: dbSite,
 };
